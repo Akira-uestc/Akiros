f42383da3785f026c3e68c5f7233357888d8a1a8
Akira-uestc <akira.uestc@gmail.com>
2024-10-22 23:22:16 +0800

    style: code indentation

--- Akishell/cmd.c
+++ Akishell/cmd.c
@@ -9,53 +9,60 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *get_cmd(char *cmd_buffer, int buffer_size) {
-  if (fgets(cmd_buffer, buffer_size, stdin) != NULL) {
-    size_t len = strlen(cmd_buffer);
-    if (len > 0 && cmd_buffer[len - 1] == '\n') {
-      cmd_buffer[len - 1] = '\0';
+char *get_cmd(char *cmd_buffer, int buffer_size) 
+{
+    if (fgets(cmd_buffer, buffer_size, stdin) != NULL) 
+    {
+        size_t len = strlen(cmd_buffer);
+        if (len > 0 && cmd_buffer[len - 1] == '\n') {
+        cmd_buffer[len - 1] = '\0';
     }
     return cmd_buffer;
-  }
-  return NULL;
+    }
+    return NULL;
 }
 
-Command *parse_cmd(char *cmd_buffer) {
-  Command *head = NULL;
-  Command *current = NULL;
-  char *token = strtok(cmd_buffer, "|");
+Command *parse_cmd(char *cmd_buffer) 
+{
+    Command *head = NULL;
+    Command *current = NULL;
+    char *token = strtok(cmd_buffer, "|");
 
-  while (token != NULL) {
-    Command *cmd = malloc(sizeof(Command));
-    cmd->next = NULL;
+    while (token != NULL) 
+    {
+        Command *cmd = malloc(sizeof(Command));
+        cmd->next = NULL;
 
-    char *prog_token = strtok(token, " ");
-    cmd->program = strdup(prog_token);
+        char *prog_token = strtok(token, " ");
+        cmd->program = strdup(prog_token);
 
-    int arg_count = 0;
-    char *arg_list[100];
-    while (prog_token != NULL) {
-      arg_list[arg_count++] = strdup(prog_token);
-      prog_token = strtok(NULL, " ");
+        int arg_count = 0;
+        char *arg_list[100];
+        while (prog_token != NULL) {
+            arg_list[arg_count++] = strdup(prog_token);
+            prog_token = strtok(NULL, " ");
+        }
+        arg_list[arg_count] = NULL;
+        cmd->args = malloc(sizeof(char *) * (arg_count + 1));
+        memcpy(cmd->args, arg_list, sizeof(char *) * (arg_count + 1));
+
+        if (head == NULL) 
+        {
+            head = cmd;
+        } 
+        else 
+        {
+            current->next = cmd;
+        }
+        current = cmd;
+
+        token = strtok(NULL, "|");
     }
-    arg_list[arg_count] = NULL;
-    cmd->args = malloc(sizeof(char *) * (arg_count + 1));
-    memcpy(cmd->args, arg_list, sizeof(char *) * (arg_count + 1));
-
-    if (head == NULL) {
-      head = cmd;
-    } else {
-      current->next = cmd;
-    }
-    current = cmd;
-
-    token = strtok(NULL, "|");
-  }
 
   return head;
 }
 
-void free_buffer(char* cmd_buffer, Command* current_cmd)
+void free_buffer(char *cmd_buffer, Command *current_cmd) 
 {
     free(cmd_buffer);
     free(current_cmd);
